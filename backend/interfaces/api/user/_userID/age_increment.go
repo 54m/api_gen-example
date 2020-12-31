2a4ce07caffdb3eddb51f5b54b210c3a03624889
54m <54m@tensi.org>
2020-12-31 21:43:25 +0900

    add(backend): add godoc

--- backend/interfaces/api/user/_userID/age_increment.go
+++ backend/interfaces/api/user/_userID/age_increment.go
@@ -5,10 +5,12 @@ import (
 	"github.com/54m/api_gen-example/backend/domain/werror"
 )
 
+// PostAgeIncrementRequest - user age increment request
 type PostAgeIncrementRequest struct {
 	ID string `json:"userID" param:"userID" validate:"required"`
 }
 
+// PostAgeIncrementResponse - user age increment response
 type PostAgeIncrementResponse struct {
 	Status   int                   `json:"status"`
 	User     *model.User           `json:"payload,omitempty"`
