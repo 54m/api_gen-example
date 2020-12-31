7070e50a23f9701d2d9de49fefe12c4ed10e8f74
54m <54m@tensi.org>
2020-12-31 21:24:36 +0900

    update(backend): user deletion controller

--- backend/interfaces/api/user/delete_controller_gen.go
+++ backend/interfaces/api/user/delete_controller_gen.go
@@ -3,8 +3,13 @@
 package user
 
 import (
+	"net/http"
+
+	"github.com/54m/api_gen-example/backend/domain/werror"
 	"github.com/54m/api_gen-example/backend/interfaces/props"
+	"github.com/54m/api_gen-example/backend/interfaces/wrapper"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/xerrors"
 )
 
 // DeleteController ...
@@ -33,16 +38,25 @@ func NewDeleteController(cp *props.ControllerProps) *DeleteController {
 func (d *DeleteController) Delete(
 	c echo.Context, req *DeleteRequest,
 ) (res *DeleteResponse, err error) {
-	// API Error Usage: github.com/54m/api_gen-example/backend/interfaces/wrapper
-	//
-	// return nil, wrapper.NewAPIError(http.StatusBadRequest)
-	//
-	// return nil, wrapper.NewAPIError(http.StatusBadRequest).SetError(err)
-	//
-	// body := map[string]interface{}{
-	// 	"code": http.StatusBadRequest,
-	// 	"message": "invalid request parameter.",
-	// }
-	// return nil, wrapper.NewAPIError(http.StatusBadRequest, body).SetError(err)
-	panic("require implements.") // FIXME require implements.
+	var werr *werror.ErrorResponse
+
+	if err = c.Validate(req); err != nil {
+		werr = werror.NewValidationError(err)
+		return nil, wrapper.NewAPIError(werr.Status, werr).SetError(err)
+	}
+
+	ctx := c.Request().Context()
+
+	if err = d.UserService.DeleteByID(ctx, req.ID); err != nil {
+		if xerrors.As(err, &werr) {
+			return nil, wrapper.NewAPIError(werr.Status, werr).SetError(err)
+		}
+		return nil, wrapper.NewAPIError(http.StatusInternalServerError).SetError(err)
+	}
+
+	res = &DeleteResponse{
+		Status: http.StatusOK,
+	}
+
+	return res, nil
 }
